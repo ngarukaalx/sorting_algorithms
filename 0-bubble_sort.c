87fc48a8c30487303a56f8d8550979cec37884d7
ngarukaalx <hiramkabuagi@gmail.com>
2023-09-23 19:55:01 +0000

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,21 +25,21 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned long int i, j;
 
-	bool swapped;
+	int swapped;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = false;
+		swapped = 1;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
-				swapped = true;
+				swapped = 0;
 				print_array(array, size);
 			}
 		}
-		if (swapped == false)
+		if (swapped == 1)
 		{
 			break;
 		}
--- sort.h
+++ sort.h
@@ -2,7 +2,6 @@
 #define SORT_H
 #include <stdio.h>
 #include <stddef.h>
-#include <stdbool.h>
 
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
